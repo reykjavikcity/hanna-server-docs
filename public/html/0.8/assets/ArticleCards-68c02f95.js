ce0dceaaa4e69e0c9dd6ca75ec2bc968c5dec5a5
Már Örlygsson <mar.nospam@anomy.net>
2026-02-09 10:45:30 +0000

    release(html-storybook): v0.8.41 (fixup)

---
+++ public/html/0.8/assets/ArticleCards-68c02f95.js
@@ -0,0 +1,10 @@
+import{j as a}from"./jsx-runtime-f961835c.js";import{C as t}from"./_CardList-30df1b95.js";const r=e=>a.jsx(t,{bemPrefix:"ArticleCards",modifier:e.size==="large"?"large":void 0,standalone:!0,cards:e.cards,imgPlaceholder:e.imgPlaceholder||!0,wrapperProps:e.wrapperProps});try{r.displayName="ArticleCards",r.__docgenInfo={description:"",displayName:"ArticleCards",props:{cards:{defaultValue:null,description:"",name:"cards",required:!0,type:{name:"ImageCardProps[]"}},imgPlaceholder:{defaultValue:null,description:"",name:"imgPlaceholder",required:!1,type:{name:"string | boolean"}},size:{defaultValue:null,description:"",name:"size",required:!1,type:{name:"enum",value:[{value:'"large"'},{value:'"normal"'}]}},wrapperProps:{defaultValue:null,description:`Custom HTML attributes for the component's wrapper element.
+
+Note, however, that some props may be intentionally
+excluded from the list.
+
+__WARNING:__
+In some cases props added this way can break the component, og hurt its
+accessibility.  Also, some props may get ignored, or over-ridden by the
+component. User discretion is advised.`,name:"wrapperProps",required:!1,type:{name:'HTMLProps<"ul", never>'}}}}}catch{}export{r as A};
+//# sourceMappingURL=ArticleCards-68c02f95.js.map
---
+++ public/html/latest/assets/ExtraLinks.stories-eb7b7ae7.js
@@ -0,0 +1,13 @@
+import{j as o}from"./jsx-runtime-f961835c.js";import{r as n}from"./range-dbab87d5.js";import{E as m}from"./ExtraLinks-ff7fc210.js";import{t as p}from"./knobs-68ae593a.js";import"./index-f80c8c95.js";import"./getSVGtext-6e71cb30.js";import"./_CardList-30df1b95.js";import"./_Button-2ca4bd3a.js";import"./_Link-b7c9c4c9.js";import"./_Image-9b5340ce.js";import"./useGetSVGtext-0a2d93d5.js";import"./hannavars-b191aae6.js";import"./ObjectHelpers-f7b28faf.js";import"./assets-a330961d.js";import"./themes-56ee0168.js";const I={title:"ExtraLinks"},l="Extra links top",d=n(1,8).map(()=>({title:"Eignaskiptayfirlýsing",href:"",summary:"Lögboðinn skriflegur gerningur um skiptingu fjöleignarhúss."})),g=["pdf","text","link"],k="Tengt efni",L=n(1,6).map(r=>({label:`Stefna í málefnum eldri borgara til ársins 2022 ${r}`,href:"",type:g[(r-1)%4]})),c=r=>{const t=r.showRelatedLinks||void 0;return o.jsx(m,{title:l,cards:d,relatedTitle:t&&k,relatedLinks:t&&L})},e={render:r=>o.jsx(c,{...r}),argTypes:{showRelatedLinks:{name:'Show "related" links'},...p},args:{showRelatedLinks:!1}};var s,a,i;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  render: args => <ExtraLinksStory {...args} />,
+  argTypes: {
+    showRelatedLinks: {
+      name: 'Show "related" links'
+    },
+    ...themeArgTypes
+  },
+  args: {
+    showRelatedLinks: false
+  }
+}`,...(i=(a=e.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};const v=["_ExtraLinks"];export{e as _ExtraLinks,v as __namedExportsOrder,I as default};
+//# sourceMappingURL=ExtraLinks.stories-eb7b7ae7.js.map
